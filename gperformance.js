2c1134fb23aa4ec141fd0a2b29e9641e045959a4
gusta-dotcom <gustavogusta438@gmail.com>
2026-03-16 14:20:04 -0300

    Add files via upload

---
+++ gperformance.js
@@ -0,0 +1,12 @@
+ 
+// let texto;
+// texto = 'Você aceita os termos do página?';
+// console.log;
+// alert(texto);
+
+// document.body.addEventListener('load', function() {
+//     window.location.href = 'teste.html';
+// }, { once: true });
+
+
+
